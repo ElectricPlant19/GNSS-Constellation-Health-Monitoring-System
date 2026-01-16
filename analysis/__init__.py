a5a497beb4303247ee1503019bbeca6f0260e425
Aditya Jhunjhunwala <109202470+ElectricPlant19@users.noreply.github.com>
2026-01-17 01:36:09 +0530

    Update __init__.py

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -4,7 +4,7 @@ Analysis modules for satellite health and orbital analysis.
 
 from .health_assessment import assess_satellite_health_with_drift, analyze_maneuver_pattern
 from .drift_analysis import calculate_longitudinal_drift, assess_drift_health, calculate_drift_trend
-from .maneuver_detection import detect_maneuvers, calculate_maneuver_uniformity
+from .maneuver_detection import detect_navik_maneuvers, calculate_maneuver_uniformity
 from .dop_calculations import calculate_dop_for_location, calculate_bounding_boxes
 
 __all__ = [
@@ -13,7 +13,7 @@ __all__ = [
     'calculate_longitudinal_drift',
     'assess_drift_health',
     'calculate_drift_trend',
-    'detect_maneuvers',
+    'detect_navik_maneuvers',
     'calculate_maneuver_uniformity',
     'calculate_dop_for_location',
     'calculate_bounding_boxes'
